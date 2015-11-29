12cff207acd2275cd8444796485b2c089d1cb843
d-mozulyov <softforyou@inbox.ru>
2015-11-30 01:45:59 +0300

    CachedSerializer: next step

--- utilities/CachedSerializer/CachedSerializer.dpr
+++ utilities/CachedSerializer/CachedSerializer.dpr
@@ -14,46 +14,110 @@ program CachedSerializer;
   {$WARN UNSAFE_CODE OFF}
   {$WARN UNSAFE_TYPE OFF}
   {$WARN UNSAFE_CAST OFF}
+  {$WARN SYMBOL_PLATFORM OFF}
 {$endif}
 {$O+}{$R-}{$I-}{$Q-}{$W-}
 
-uses {$ifdef UNITSCOPENAMES}
-       System.SysUtils,
-     {$else}
-       SysUtils,
-     {$endif}
+uses {$ifdef UNITSCOPENAMES}Winapi.Windows{$else}Windows{$endif},
+     {$ifdef UNITSCOPENAMES}System.SysUtils{$else}SysUtils{$endif},
+     UniConv,
      SerializeUnit in 'SerializeUnit.pas';
 
+
+procedure SetClipboardText(const Text: UnicodeString);
 var
-  Serializer: TSerializer;
-  ParamIndex: Integer;
-  FileName, S: string;
-
+  Size: NativeUInt;
+  Handle: HGLOBAL;
+  Ptr: Pointer;
 begin
+  OpenClipboard(0);
   try
-    FileName := ParamStr(1);
+    Size := (Length(Text) + 1) * SizeOf(WideChar);
+    Handle := GlobalAlloc(GMEM_DDESHARE or GMEM_MOVEABLE, Size);
+    try
+      Win32Check(Handle <> 0);
+      Ptr := GlobalLock(Handle);
+      Win32Check(Assigned(Ptr));
+      Move(PUnicodeChar(Text)^, Ptr^, Size);
+      GlobalUnlock(Handle);
+      SetClipboardData(CF_UNICODETEXT, Handle);
+    finally
+      GlobalFree(Handle);
+    end;
+  finally
+    CloseClipboard;
+  end;
+end;
 
-    if (not FileExists(FileName)) then
+var
+  FlagWait, FlagCopy: Boolean;
+  ParamIndex, i: Integer;
+  FileName, S: string;
+  Parameters: TSerializeParameters;
+  Serializer: TSerializer;
+  List: TUnicodeStrings;
+  Text: UnicodeString;
+begin
+  // check flags: -nowait, -nocopy
+  FlagWait := True;
+  FlagCopy := True;
+  ParamIndex := 1;
+  repeat
+    S := ParamStr(ParamIndex);
+    if (S = '') then Break;
+
+    if (S = '-nowait') then FlagWait := False;
+    if (S = '-nocopy') then FlagCopy := False;
+  until (False);
+
+  // load file
+  FileName := ParamStr(1);
+  try
+    if (FileExists(FileName)) then
     begin
-      // some message todo
-    end else
-    begin
-      Serializer := TSerializer.Create(FileName);
+      Parameters.AddFromFile(FileName, True);
+
+      // update parameters
+      ParamIndex := 2;
+      repeat
+        S := ParamStr(ParamIndex);
+        if (S = '') then Break;
+
+        if (not Parameters.ParseParameter(S)) then
+        begin
+          if (S <> '-nowait') and (S <> '-nocopy') then
+            Writeln('Unknown parameter "', S, '"');
+        end;
+      until (False);
+
+      // serialize
+      Serializer := TSerializer.Create;
       try
-        // update parameters
-        ParamIndex := 2;
-        repeat
-          S := ParamStr(ParamIndex);
-          if (S = '') then Break;
+        List := Serializer.Process(Parameters);
 
-          // todo
-        until (False);
+        // collect list, display to the console
+        for i := 0 to Length(List) - 1 do
+        begin
+          if (Text <> '') then Text := Text + #13#10;
+          Text := Text + List[i];
 
-        // process serializing
-        // todo
+          Writeln(List[i]);
+        end;
+
+        // copy to the clipboard
+        if (FlagCopy) then
+        begin
+          SetClipboardText(Text);
+          Writeln;
+          Writeln('The code has been successfully copied to the clipboard');
+        end;
       finally
         Serializer.Free;
       end;
+    end else
+    begin
+      // some message todo
+
     end;
   except
     on EAbort do ;
@@ -65,7 +129,7 @@ begin
     end;
   end;
 
-  if (ParamStr(ParamCount) <> '-nowait') then
+  if (FlagWait) then
   begin
     Writeln;
     Write('Press Enter to quit');
--- utilities/CachedSerializer/SerializeUnit.pas
+++ utilities/CachedSerializer/SerializeUnit.pas
@@ -28,64 +28,212 @@ type
   TUnicodeStrings = array of UnicodeString;
   PUnicodeStrings = ^TUnicodeStrings;
 
-type
+  TParameterOptions = record
+    Name: UnicodeString;
+    Count: NativeUInt;
+    O1, O2, O3: UnicodeString;
+  end;
+
+{ TSerializeParameters record }
+
+  TSerializeParameters = {$ifdef OPERATORSUPPORT}record{$else}object{$endif}
+  private
+    FCount: NativeUInt;
+    FItems: TUnicodeStrings;
+
+    function ParseOptions(var Options: TParameterOptions; const S: UnicodeString): Boolean;
+    procedure ParseParametersLine(const S: UTF16String);
+  public
+    function ParseParameter(const S: UnicodeString): Boolean;
+    function Add(const S: UnicodeString): NativeUInt;
+    procedure Delete(const AFrom: NativeUInt; const ACount: NativeUInt = 1);
+    procedure AddFromFile(const FileName: string; const ParametersLine: Boolean);
+
+    property Count: NativeUInt read FCount;
+    property Items: TUnicodeStrings read FItems;
+  end;
+
+
+{ TSerializer class }
+
   TSerializer = class(TObject)
   private
-    FFileName: string;
 
-  protected
-    function InitializeParameters(const S: UnicodeString): Boolean;
-    procedure AddEntity(const S: UnicodeString);
   public
-    constructor Create(const AFileName: string);
-    destructor Destroy; override;
-
-    property FileName: string read FFileName;
+    function Process(const Parameters: TSerializeParameters): TUnicodeStrings;
   end;
+
+
 
 
 implementation
 
-{ TSerializer }
 
-constructor TSerializer.Create(const AFileName: string);
+{ TSerializeParameters }
+
+function TSerializeParameters.Add(const S: UnicodeString): NativeUInt;
+begin
+  Result := FCount;
+  Inc(FCount);
+  SetLength(FItems, FCount);
+  FItems[Result] := S;
+end;
+
+procedure TSerializeParameters.Delete(const AFrom, ACount: NativeUInt);
+var
+  L, i: NativeUInt;
+begin
+  L := FCount;
+  if (AFrom < L) and (ACount <> 0) then
+  begin
+    Dec(L, AFrom);
+    if (L <= ACount) then
+    begin
+      L := AFrom;
+    end else
+    begin
+      Inc(L, AFrom);
+      Dec(L, ACount);
+
+      for i := 0 to (L - AFrom - 1) do
+        FItems[AFrom + i] := FItems[AFrom + ACount + i];
+    end;
+
+    FCount := L;
+    SetLength(FItems, L);
+  end;
+end;
+
+function TSerializeParameters.ParseOptions(var Options: TParameterOptions;
+  const S: UnicodeString): Boolean;
+var
+  P: NativeInt;
+  Str: UTF16String;
+
+  function ParseOption(var O: UnicodeString): Boolean;
+  begin
+    Result := True;
+
+    P := Str.CharPos(':');
+    if (P = 0) then
+    begin
+      Result := False;
+      Exit;
+    end;
+
+    Inc(Options.Count);
+    if (P < 0) then
+    begin
+      O := Str.ToUnicodeString;
+      Str.Length := 0;
+      Exit;
+    end;
+
+    O := Str.SubString(P).ToUnicodeString;
+    Str.Offset(P + 1);
+  end;
+begin
+  Result := False;
+  Options.Count := 0;
+  Str.Assign(S);
+
+  P := Str.CharPos('"');
+  if (P < 0) then
+  begin
+    Options.Name := Str.ToUnicodeString;
+    Result := True;
+    Exit;
+  end else
+  begin
+    if (P = 0) then Exit;
+    Options.Name := Str.SubString(0, P).ToUnicodeString;
+
+    Str.Offset(P);
+    P := Str.CharPos('"');
+    if (P < 0) or (NativeUInt(P) <> Str.Length - 1) then Exit;
+    Str.Length := Str.Length - 1;
+
+    if (not ParseOption(Options.O1)) then Exit;
+    if (Str.Length <> 0) then
+    begin
+      if (not ParseOption(Options.O2)) then Exit;
+
+      if (Str.Length <> 0) then
+      begin
+        if (not ParseOption(Options.O3)) then Exit;
+        if (Str.Length <> 0) then Exit;
+      end;
+    end;
+
+    Result := True;
+  end;
+end;
+
+function TSerializeParameters.ParseParameter(const S: UnicodeString): Boolean;
+var
+  Options: TParameterOptions;
+begin
+  Result := False;
+  if (not ParseOptions(S)) then Exit;
+
+
+
+
+
+
+  Result := False;
+end;
+
+procedure TSerializeParameters.ParseParametersLine(const S: UTF16String);
+var
+  Str: UTF16String;
+  Buf: UnicodeString;
+  L: NativeUInt;
+begin
+  Str := S;
+  while (not Str.Trim) do
+  begin
+    L := 0;
+    while (L < Str.Length) and (Str.Chars[L] <= ' ') do
+    begin
+      Inc(L);
+    end;
+
+    Buf := Str.SubString(L).ToUnicodeString;
+    if (not ParseParameter(Buf)) then
+      raise Exception.CreateFmt('Unknown file parameter "%s"', [Buf]);
+
+    Str.Offset(L);
+  end;
+end;
+
+procedure TSerializeParameters.AddFromFile(const FileName: string;
+  const ParametersLine: Boolean);
 var
   Text: TUTF16TextReader;
   S: UTF16String;
 begin
-  inherited Create;
-  FFileName := AFileName;
-
-  Text := TUTF16TextReader.CreateFromFile(AFileName);
+  Text := TUTF16TextReader.CreateFromFile(FileName);
   try
-    if (Text.Readln(S)) then
-    begin
-      if (not InitializeParameters(S.ToUnicodeString)) then
-        AddEntity(S.ToUnicodeString);
-    end;
+    if (ParametersLine) and (Text.Readln(S)) then
+      ParseParametersLine(S);
 
     while (Text.Readln(S)) do
-    begin
-      AddEntity(S.ToUnicodeString);
-    end;
+      Add(S.ToUnicodeString);
   finally
     Text.Free;
   end;
 end;
 
-destructor TSerializer.Destroy;
-begin
 
-  inherited;
-end;
 
-function TSerializer.InitializeParameters(const S: UnicodeString): Boolean;
-begin
-  Result := False;
-end;
 
-procedure TSerializer.AddEntity(const S: UnicodeString);
+{ TSerializer }
+
+function TSerializer.Process(
+  const Parameters: TSerializeParameters): TUnicodeStrings;
 begin
+  Result := nil;
 
 end;
 
